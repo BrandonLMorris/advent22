1d5b960b2f441cb9ab4facd260ad1f47e90051f0
Brandon Morris <git@brandonmorris.dev>
2022-12-30 12:23:08 -0800

    Improve efficiency of part 2 solution to day 12

--- aoc22/days/Day12.swift
+++ aoc22/days/Day12.swift
@@ -14,27 +14,23 @@ internal struct Day12: Day {
   }
 }
 
-fileprivate struct AltitudeMap {
+private struct AltitudeMap {
   private let altitudes: [[Int]]
   private let start: Point
   private let end: Point
-  private let lowestPoints: [Point]
 
   lazy var distanceToTop: Int = {
-    computeDistance(from: start)
+    computeDistance(from: start, toPointWhere: { $0 == end })
   }()
-  
+
   lazy var shortestCompleteDistance: Int = {
-    // Note: This is *exteremely* inefficient, since we're effectively recalcualting
-    // distances for lots of points multiple times.
-    lowestPoints.map({ computeDistance(from: $0) }).filter({ $0 > 0 }).min()!
+    computeDistance(from: end, toPointWhere: { elevation(at: $0) == 0 }, traveling: .down)
   }()
 
   init(lines: [String]) {
     var start: Point? = nil
     var end: Point? = nil
     var alts: [[Int]] = []
-    var starts: [Point] = []
 
     for row in 0..<lines.count {
       var altRow: [Int] = []
@@ -44,17 +40,6 @@ fileprivate struct AltitudeMap {
 
         let rowIdx = line.index(line.startIndex, offsetBy: col)
         altRow.append(line[rowIdx].toAltitude())
-        switch line[rowIdx] {
-        case "S":
-          start = Point(row: row, col: col)
-          starts.append(Point(row: row, col: col))
-        case "a":
-          starts.append(Point(row: row, col: col))
-        case "E":
-          end = Point(row: row, col: col)
-        default:
-          break
-        }
         if line[rowIdx] == "S" { start = Point(row: row, col: col) }
         if line[rowIdx] == "E" { end = Point(row: row, col: col) }
       }
@@ -64,10 +49,16 @@ fileprivate struct AltitudeMap {
     altitudes = alts
     self.start = start!
     self.end = end!
-    lowestPoints = starts
   }
 
-  private func computeDistance(from startPoint: Point) -> Int {
+  private enum TravelDirection {
+    case up, down
+  }
+
+  private func computeDistance(
+    from startPoint: Point, toPointWhere matcher: (Point) -> Bool,
+    traveling direction: TravelDirection = .up
+  ) -> Int {
     var visited = Set<Point>()
     var toVisit = [startPoint]
     var distances: [Point: Int] = [startPoint: 0]
@@ -76,26 +67,38 @@ fileprivate struct AltitudeMap {
       let current = toVisit.removeFirst()
       if visited.contains(current) { continue }
       visited.insert(current)
-      for newNeighbor in neighbors(for: current).subtracting(visited) {
+
+      for newNeighbor in neighbors(for: current, going: direction).subtracting(visited) {
         toVisit.append(newNeighbor)
-        distances[newNeighbor] = distances[current]! + 1
+        if matcher(newNeighbor) {
+          return distances[current]! + 1
+        } else {
+          distances[newNeighbor] = distances[current]! + 1
+        }
       }
     }
 
-    return distances[end] ?? -1
+    // Indicates that no such path is possible.
+    return -1
   }
 
-  private func neighbors(for point: Point) -> Set<Point> {
+  private func neighbors(for point: Point, going direction: TravelDirection) -> Set<Point> {
     let result = [
       Point(row: point.row - 1, col: point.col),  // Up
       Point(row: point.row + 1, col: point.col),  // Down
       Point(row: point.row, col: point.col - 1),  // Left
       Point(row: point.row, col: point.col + 1),  // Right
     ].filter({ p in
-      p.row >= 0 && p.col >= 0 &&
-      p.row < altitudes.count &&
-      p.col < altitudes[0].count &&
-      elevation(at: p) - elevation(at: point) <= 1
+      // Ensure the new neighbor is in bounds.
+      p.row >= 0 && p.col >= 0 && p.row < altitudes.count && p.col < altitudes[0].count
+    }).filter({ p in
+      // Ensure the new neighbor is within one level of altitude, depending on direction.
+      switch direction {
+      case .up:
+        return elevation(at: p) - elevation(at: point) <= 1
+      case .down:
+        return elevation(at: point) - elevation(at: p) <= 1
+      }
     })
     return Set(result)
   }
@@ -109,8 +112,8 @@ fileprivate struct AltitudeMap {
   }
 }
 
-fileprivate extension Character {
-  func toAltitude() -> Int {
+extension Character {
+  fileprivate func toAltitude() -> Int {
     let zeroAlt = Int(Character("a").asciiValue!)
     switch self {
     case "S":
